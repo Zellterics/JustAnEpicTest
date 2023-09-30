bff1fdde015459a46fcd7d881940e2656c65a52b
Zellteric <alejandropinzon106@gmail.com>
2023-09-30 02:13:47 -0600

    Add project files.

---
+++ JustAnEpicTest/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JustAnEpicTest
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //temp func
+            String[] lines;
+            lines = System.IO.File.ReadAllLines("Map.csv");
+            foreach (string line in lines) {
+                Console.WriteLine(line);
+            }
+            Console.ReadLine();
+        }
+    }
+}
